046e513ff198bbd6091ac663cf428dee1c2aead6
Nick Murphy <nickmurphy@MACLAP-9MDMD6M-58777.local>
2020-02-09 19:35:27 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: 'http://192.168.86.130:5000'
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on('buttonPush', data => this.setState({ response: data }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+        <div style={{ textAlign: 'center' }}>
+          {response === 1
+            ? <p>The light is on</p>
+            : <p>The light is off</p>}
+        </div>
+    );
+  }
 }
 
 export default App;
